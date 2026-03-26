f2e219fb0b4de9435a970685c6208b71cd3ca8b1
RODRIGO LUIZ DE SOUSA RIBEIRO <rodrigo_ls_ribeiro@estudante.sesisenai.org.br>
2026-03-25 22:22:43 -0300

    Adicionando arquivos no github!

---
+++ LivrariaOnline/src/test/java/org/example/livrariaonline/LivrariaOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.livrariaonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivrariaOnlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
